e6b0e70ca48c38906b4d5ba06850255f3bbe42e4
Triali <105738086+Triali@users.noreply.github.com>
2023-02-13 17:52:41 -0700

    the majority of the code

--- DatalogProgram.h
+++ DatalogProgram.h
@@ -6,15 +6,81 @@
 #define CS236PROJ1_DATALOGPROGRAM_H
 
 #include "Rule.h"
+#include "Predicate.h"
+#include <set>
 
 using namespace std;
 
 class DatalogProgram
 {
 private:
+    vector<Predicate> schemes;
+    vector<Predicate> facts;
+    vector<Rule> rules;
+    vector<Predicate> queries;
 
+    set<string> domain;
 
 public:
+    void addScheme(Predicate newScheme)
+    {
+        schemes.push_back(newScheme);
+    }
+
+    void addFact(Predicate newFact)
+    {
+        facts.push_back(newFact);
+    }
+
+    void addRule(Rule newRule)
+    {
+        rules.push_back(newRule);
+    }
+
+    void addQuery(Predicate newQuery)
+    {
+        queries.push_back(newQuery);
+    }
+
+    string DatalogProgramToString()
+    {
+        stringstream dpString;
+        dpString << "Schemes(" << schemes.size() << "):" << endl;
+        dpString << PredVectToString(schemes);
+
+        dpString << "Facts(" << facts.size() << "):" << endl;
+        dpString << PredVectToString(facts);
+
+        dpString << "Rules(" << rules.size() << "):" << endl;
+        dpString << RuleVectToString(rules);
+
+        dpString << "Queries(" << queries.size() << "):" << endl;
+        dpString << PredVectToString(queries);
+
+        dpString << "Domain(" << "#" << "):" << endl;
+
+        return dpString.str();
+    }
+
+    string PredVectToString(vector<Predicate> predVect)
+    {
+        stringstream predString;
+        for (int i = 0; i < predVect.size(); ++i)
+        {
+            predString << predVect.at(i).PredicateToString()<< endl;
+        }
+        return predString.str();
+    }
+
+    string RuleVectToString(vector<Rule> ruleVect)
+    {
+        stringstream ruleString;
+        for (int i = 0; i < ruleVect.size(); ++i)
+        {
+            ruleString << ruleVect.at(i).RuleToString();
+        }
+        return ruleString.str();
+    }
 
 
 };
