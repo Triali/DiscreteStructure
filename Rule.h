44c3c6e836392da15e99385c4db32423763a922c
Triali <105738086+Triali@users.noreply.github.com>
2023-02-13 17:10:56 -0700

    Rule.h

--- Rule.h
+++ Rule.h
@@ -11,9 +11,44 @@ using namespace std;
 class Rule
 {
 private:
+    vector<Predicate> predicates;
 
 
 public:
+    //constructor
+    Rule(Predicate HP, Predicate FP)
+    {
+        predicates.push_back(HP);
+        predicates.push_back(FP);
+//        cout << "finished Constructor" << endl;
+
+    }
+
+    void addPredicate(Predicate newPredicate)
+    {
+//        cout << "running addPredicate" << endl;
+        predicates.push_back(newPredicate);
+    }
+
+    string RuleToString()
+    {
+//        cout << "running RuleToString" << endl;
+        stringstream ruleString;
+        ruleString << "  " << predicates.at(0).PredicateToStringR();
+        ruleString << " :- ";
+        for (int i = 1; i < predicates.size(); ++i)
+        {
+//            add the parameter
+            ruleString << predicates.at(i).PredicateToStringR();
+//            add comma if not at the last index of vector
+            if (i != predicates.size()-1)
+            {
+                ruleString << ",";
+            }
+        }
+        ruleString << ".";
+        return ruleString.str();
+    }
 
 
 };
