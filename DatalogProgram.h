ba8e735f6faabb0fd3d0d68fc74c492e95ed6289
Triali <105738086+Triali@users.noreply.github.com>
2023-02-13 17:11:29 -0700

    Add files via upload

---
+++ DatalogProgram.h
@@ -0,0 +1,22 @@
+//
+// Created by elton on 2/10/2023.
+//
+
+#ifndef CS236PROJ1_DATALOGPROGRAM_H
+#define CS236PROJ1_DATALOGPROGRAM_H
+
+#include "Rule.h"
+
+using namespace std;
+
+class DatalogProgram
+{
+private:
+
+
+public:
+
+
+};
+
+#endif //CS236PROJ1_DATALOGPROGRAM_H
